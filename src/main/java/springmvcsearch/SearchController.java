92f27678bc290985389a421c2f3bd6361bb1ab68
Golu Vishwakarma <golu.v@techouts.com>
2023-01-23 18:50:07 +0530

    practicle

--- src/main/java/springmvcsearch/SearchController.java
+++ src/main/java/springmvcsearch/SearchController.java
@@ -1,12 +1,22 @@
 package springmvcsearch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SearchController {
+	
+	@RequestMapping("/user/{userId}")
+	public String getUserDetails(@PathVariable("userId") int userId
+			, @PathVariable("userName") String userName) {
+		
+		System.out.println(userId);
+		System.out.println(userName);
+		return "home";
+	}
 	
 	@RequestMapping("/home")
 	public String home() {
